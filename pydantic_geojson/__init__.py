5e661960d39d79f44e2f0e048b9dbcbc78bd9a82
rob dailey <rob@suspected.org>
2023-07-04 16:15:10 -0400

    Run through `black` for uniformity

--- pydantic_geojson/__init__.py
+++ pydantic_geojson/__init__.py
@@ -9,30 +9,27 @@ from .object_type import GeometryType
 from .point import PointModel
 from .polygon import PolygonModel
 
-__author__ = 'Aliaksandr Vaskevich'
+__author__ = "Aliaksandr Vaskevich"
 __maintainer__ = __author__
 
-__email__ = 'vaskevic.an@gmail.com'
-__license__ = 'MIT'
+__email__ = "vaskevic.an@gmail.com"
+__license__ = "MIT"
 
 __all__ = [
-    '__author__',
-    '__email__',
-    '__license__',
-    '__maintainer__',
-
+    "__author__",
+    "__email__",
+    "__license__",
+    "__maintainer__",
     # object type
-    'GeometryType',
-
+    "GeometryType",
     # models
-    'PointModel',
-    'MultiPointModel',
-    'LineStringModel',
-    'MultiLineStringModel',
-    'PolygonModel',
-    'MultiPolygonModel',
-    'GeometryCollectionModel',
-    'FeatureModel',
-    'FeatureCollectionModel',
-
+    "PointModel",
+    "MultiPointModel",
+    "LineStringModel",
+    "MultiLineStringModel",
+    "PolygonModel",
+    "MultiPolygonModel",
+    "GeometryCollectionModel",
+    "FeatureModel",
+    "FeatureCollectionModel",
 ]
